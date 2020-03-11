94ea7f8751c9a0ee779853e755edb170290fa7d3
Olena Kostiuk <helena.kostyuk@gmail.com>
2020-03-11 13:08:14 +0200

    Добавьте файлы проекта.

---
+++ Rental/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer("Alex");
+            Movie movie = new Movie("Matrix", 1);
+            Rental rental = new Rental(movie, 2);
+
+            customer.Rentals = rental;
+            Console.WriteLine(customer.Statement());
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rental/Rental.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rental
+{
+    /// <summary>
+    /// Класс, представляющий данные о прокате фильма.
+    /// </summary>
+    class Rental
+    {
+        // Поля
+
+        private Movie movie = null;
+        private int daysRented = 0;
+
+        // Методы
+
+        public Rental(Movie movie, int daysRented)
+        {
+            this.movie = movie;
+            this.daysRented = daysRented;
+        }
+
+        // Свойства
+
+        public Movie Movie
+        {
+            get { return this.movie; }
+        }
+
+        public int DaysRented
+        {
+            get { return this.daysRented; }
+        }
+    }
+}
